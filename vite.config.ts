11b14219d09381f8c62828df1322af078409afa8
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2026-07-09 08:21:26 +0700

    clean repo

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,61 @@ import UnoCSS from "unocss/vite";
 import Yaml from "unplugin-yaml/vite";
 import { defineConfig } from "vite-plus";
 
+const ignorePatterns = [
+  "*.min.*",
+  "*.map",
+  "**/public",
+  "**/build",
+  "**/dist",
+  "**/out",
+  "**/.github",
+  "**/.next",
+  "**/.astro",
+  "**/.netlify",
+  "**/*.gen.*",
+];
+
 export default defineConfig({
-  base: "./",
-  plugins: [vue(), UnoCSS(), Yaml()],
+  staged: {
+    "*": "vp check --fix",
+  },
+  fmt: {
+    ignorePatterns,
+    sortImports: {
+      groups: [
+        "type-import",
+        ["value-builtin", "value-external"],
+        "type-internal",
+        "value-internal",
+        ["type-parent", "type-sibling", "type-index"],
+        ["value-parent", "value-sibling", "value-index"],
+        "unknown",
+      ],
+    },
+  },
+  lint: {
+    plugins: [
+      "typescript",
+      "unicorn",
+      "react",
+      "react-perf",
+      "import",
+      "jsx-a11y",
+      "node",
+      "promise",
+      "vitest",
+      "vue",
+    ],
+    ignorePatterns,
+    options: {
+      typeAware: true,
+      typeCheck: true,
+    },
+  },
+  plugins: [
+    vue(), UnoCSS(), Yaml(),
+  ],
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
